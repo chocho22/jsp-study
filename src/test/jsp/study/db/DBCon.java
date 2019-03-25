e588f5ee51f182e889480688829ca76d8681f1cb
crisoberyl@naver.com <crisoberyl@naver.com>
2019-03-25 09:38:01 +0900

    1

--- src/test/jsp/study/db/DBCon.java
+++ src/test/jsp/study/db/DBCon.java
@@ -9,12 +9,11 @@ public class DBCon {
 	private static final String USER = "osfu";
 	private static final String PASSWORD = "12345678";
 	private static final String DRIVER = "oracle.jdbc.OracleDriver";
+	private static Connection con = null;
 	
 	// oracle 패키지의 jdbc의 OracleDriver라는 패키지가 있음.
 	// 오라클에 전화할 수 있는 유심칩
 	// 원래는 골라주어야 하지만, 지금은 깔려있는 것이 jdbc밖에 없어서 안써도됨.
-	
-	private static Connection con = null;
 	
 	private static void open() {
 		try {
